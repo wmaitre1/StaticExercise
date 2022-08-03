17c36a2748ff9393a1ea3cb9b694f0dcea052727
William Maitre <william.maitre@gmail.com>
2022-08-03 10:02:54 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(80); //26.66 degrees Celsius after conversion 
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(35); // 95 degrees Fahrenheit after conversion 
+
+            Console.WriteLine("After required conversion");
+
+            Console.WriteLine("-----------------------------------");
+
+            Console.WriteLine($"celsius {celsius}");
+
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
---
+++ TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var result = (fahrenheit - 32) / 1.8;
+
+            return result;
+        }
+       
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32; 
+        }
+    }
+}
